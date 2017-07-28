3ed4c9115ceb8486a37d3c73458de64e6bb967dd
yizy <zy473@nyu.edu>
2017-07-27 23:31:40 -0700

    Create SingleNumber.py

---
+++ leetcode/python/SingleNumber.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/single-number
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for n in nums:
+            res ^= n
+        return res
